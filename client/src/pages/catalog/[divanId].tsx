22583451f399db768e7c2ff12452340350dc13a2
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-08-01 13:06:14 +0600

    404

--- client/src/pages/catalog/[divanId].tsx
+++ client/src/pages/catalog/[divanId].tsx
@@ -5,20 +5,29 @@ import { $divan, setDivan } from "../../../context/divan"
 import { getDivanFx } from "../../../app/api/divans"
 import { toast } from "react-toastify"
 import { setDivans } from "../../../context/divans"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import DivanPage from "../../../components/templates/DivanPage/DivanPage"
+import { useRouter } from "next/router"
+import Custom404 from "../404"
 
 function CatalogDivanPage({ query }: { query: IQueryParams }) {
 
   const divan = useStore($divan)
+  const router = useRouter()
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     loadDivan()
-  },[])
+  }, [router.asPath])
 
   const loadDivan = async () => {
     try {
       const data = await getDivanFx(`/divans/find/${query.divanId}`)
+
+      if (!data) {
+        setError(true)
+        return
+      }
 
       setDivan(data)
     } catch (error) {
@@ -28,16 +37,19 @@ function CatalogDivanPage({ query }: { query: IQueryParams }) {
 
   return (
     <>
-       <SEO
-       title={`ВАША МЕБЕЛЬ | ${divan.name}`}
+      <SEO
+        title={`ВАША МЕБЕЛЬ | ${divan.name}`}
         /* title="Каталог | МЯГКАЯ МЕБЕЛЬ ОТ ПРОИЗВОДИТЕЛЯ Ваша Мебель" */
         description="Интернет-магазин диванов в городе Новосибирске Ваша Мебель"
         keywords="недорогие диваны от производителя"
       />
       {/* <SEO {...shippingConfig} /> */}
 
-       {/* <DivanPage query={query} /> */}
-       <DivanPage />
+      {/* <DivanPage query={query} /> */}
+      {error ? (
+        <Custom404 />
+      ) : (<DivanPage />)}
+
     </>
   )
 }
